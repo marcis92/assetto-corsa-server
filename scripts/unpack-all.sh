3a40afe054bb97db58c1be0f3b44f555817a1c4e
marcis92 <76939141+marcis92@users.noreply.github.com>
2023-03-10 21:09:30 +0100

    Update unpack-all.sh

--- scripts/unpack-all.sh
+++ scripts/unpack-all.sh
@@ -6,11 +6,3 @@ if [ -f /opt/steamcmd/steamcmd.tar.gz ]; then
   tar xzf steamcmd.tar.gz
   rm steamcmd.tar.gz
 fi
-
-if [ -f /opt/stracker/stracker.zip ]; then
-  echo ">>> Installing stracker"
-  cd /opt/stracker
-  unzip -qq stracker.zip
-  tar xzf stracker_linux_x86.tgz
-  rm stracker.zip stracker_linux_x86.tgz
-fi
