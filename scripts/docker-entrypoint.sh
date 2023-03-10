f5a3cdd6fc66c34a50459e9dc3e2fa1bfb3776da
marcis92 <76939141+marcis92@users.noreply.github.com>
2023-03-10 21:13:05 +0100

    Update docker-entrypoint.sh

--- scripts/docker-entrypoint.sh
+++ scripts/docker-entrypoint.sh
@@ -4,8 +4,5 @@ export INSTALL_PATH="/steamapps/${AC_FOLDER}"
 
 install-all.sh
 
-cd $INSTALL_PATH/plugins/stracker
-nohup ./stracker_linux_x86/stracker --stracker_ini stracker.ini >/dev/null 2>&1 &
-
 cd $INSTALL_PATH
 ./acServer
